da15e1fc311d417a02900f620c8aeae19da22a87
Jess M <eveyj777@gmail.com>
2022-05-01 15:03:29 +0100

    add route and replace request

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,15 +1,16 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const config = require("config")
-const request = require("request")
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("config");
+const axios = require("axios").default;
+const { getInvestments } = require("./services");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json({limit: "10mb"}))
+app.use(bodyParser.json({ limit: "10mb" }));
 
 app.get("/investments/:id", (req, res) => {
-  const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
+  const { id } = req.params
+  axios.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
     if (e) {
       console.error(e)
       res.send(500)
@@ -17,7 +18,17 @@ app.get("/investments/:id", (req, res) => {
       res.send(investments)
     }
   })
-})
+});
+
+app.get("/admin/report", async (req, res, next) => {
+  try {
+    const investmentsData = await getInvestments();
+    res.send(investmentsData);
+  } catch (e) {
+    console.log(e);
+    return next(e)
+  }
+});
 
 app.listen(config.port, (err) => {
   if (err) {
@@ -25,4 +36,4 @@ app.listen(config.port, (err) => {
     process.exit(1)
   }
   console.log(`Server running on port ${config.port}`)
-})
+});
