77d786e12429471172161f20173460da9be66133
Jess M <eveyj777@gmail.com>
2022-05-01 15:10:45 +0100

    add test for expected output object

--- admin/__tests__/index.test.js
+++ admin/__tests__/index.test.js
@@ -1,8 +1,22 @@
-const { getInvestments } = require("../src/services");
+const { getInvestments, mapInvestments } = require("../src/services");
+const investmentsData = require("../../investments/src/data.json");
 
 describe('admin services should', () => {
     it('get investments data', async () => {
         const data = await getInvestments();
         expect(data && typeof data === 'object').toBe(true)
+    });
+    it('return an object with specfied properties', () => {
+        const outputData = mapInvestments(investmentsData);
+        outputData.forEach((outputObject) => {
+            expect(outputObject).toEqual(expect.objectContaining({
+                user: expect.any(String),
+                firstName: expect.any(String),
+                lastName: expect.any(String),
+                date: expect.any(String),
+                holding: expect.any(String),
+                value: expect.any(Number)
+            }));
+        })
     });
 });
