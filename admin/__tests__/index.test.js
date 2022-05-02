f8c8f9f8f601f83d3049bf57f90537086054f537
Jess M <eveyj777@gmail.com>
2022-05-02 21:55:00 +0100

    add company match unit test

--- admin/__tests__/index.test.js
+++ admin/__tests__/index.test.js
@@ -1,11 +1,17 @@
-const { getInvestments, mapInvestments } = require("../src/services");
+const { getInvestments, mapInvestments, matchCompany } = require("../src/services");
 const investmentsData = require("../../investments/src/data.json");
+const companiesData = require("../../financial-companies/src/data.json");
 
 describe('admin services should', () => {
     it('get investments data', async () => {
         const data = await getInvestments();
         expect(data && typeof data === 'object').toBe(true)
     });
+
+    it('matches companies by id', () => {
+        expect(matchCompany(companiesData, '1')).toMatch(/The Big Investment Company/);
+    });
+
     it('return an object with specfied properties', () => {
         const outputData = mapInvestments(investmentsData);
         outputData.forEach((outputObject) => {
